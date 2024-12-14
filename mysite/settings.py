7a4e4c45d2ec66a9441ef73d5a278426e3cdc79a
X-spec7 <mercury.spec77@gmail.com>
2024-12-15 02:42:09 +0900

    feat: add test using selenium

--- mysite/settings.py
+++ mysite/settings.py
@@ -64,6 +64,16 @@ CHANNEL_LAYERS = {
     },
 }
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+        "TEST": {
+            "NAME": BASE_DIR / "db.sqlite3",
+        },
+    }
+}
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
